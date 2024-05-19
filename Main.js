21f5c7a3257e21211001a7394a079117b868dd99
ZealotBombing <cfuentesguerrero333@gmail.com>
2024-05-19 01:19:06 -0400

    delete item

--- Main.js
+++ Main.js
@@ -1,105 +1,206 @@
-'use strict'
+
+const char = 'QWERTYUIOPASDFGHJKLZXCVBNM1234567890'
 
 window.addEventListener('load',(e)=>{
 
     let data = localStorage.getItem('taskList')
     
     let lst = JSON.parse(data)
-    
+
     ListCards(lst)
 })
 
-document.getElementById('btn_add').addEventListener('click', (e)=>{
-    
-    let input = document.getElementById('inp_task')
-    
-    let task = input.value
+document.getElementById('task_form').addEventListener('submit', (e)=>{
+
+    e.preventDefault()
+
+    let form = e.target
+
+    let fields = form.querySelectorAll('input, textarea')
+
+    let formFill = true;
+
+    fields.forEach(field => {
+
+        if(field.value === null || field.value === '' || /^\s+$/.test(field.value) ){
+            formFill = false
+            field.style.borderColor = 'red'
+        }
+        else field.style.borderColor = ''
+    })
+
+    if(!formFill) return
 
     let taskList = localStorage.getItem('taskList')
 
-    let modal = document.getElementById('exampleModal')
+    let taskId = generateId(taskList)
 
-    if(task === null || task === '' || /^\s+$/.test(task)){
-
-        input.style.borderColor = 'red'
-        return 
-    }else{
-        input.style.borderColor = ''
+    let taskItem = {
+        id: taskId,
+        title: form.title.value,
+        description: form.description.value,
+        createDate: Date.now(),
+        plannedDate: form.date.value,
+        deleteDate: null,
+        actualDate: null
     }
 
     if(!taskList){
 
         let firstList = []
-        firstList.push(task)
+        firstList.push(taskItem)
         localStorage.setItem('taskList', JSON.stringify(firstList))
         ListCards(firstList)
 
     }else{
         
         let list = JSON.parse(taskList)
-        list.push(task)
+        list.push(taskItem)
         localStorage.setItem('taskList',  JSON.stringify(list))
         ListCards(list)
     }
+    form.reset()
 
-    input.value =''
-    modal.classList.remove('show')
-    modal.style.display = 'none'
-
+    document.getElementById('btn_close').click();
+    
+    alert('Save successfully')
 })
 
 document.getElementById('btn_cancel').addEventListener('click', (e)=>{
-    document.getElementById('inp_task').value= ''
+    let form = document.getElementById('task_form')
+    form.reset()
 })
 
-document.getElementById('lst_row').addEventListener('click', (e)=>{
 
-    let btn_name = e.target.name
+document.getElementById('container_lst').addEventListener('click', (e)=>{
 
-    let data = localStorage.getItem('taskList')
-    let lst = JSON.parse(data)
+    let action;
+    let taskId;
 
-    let index = e.target.dataset.item
+    if(e.target.tagName === 'I'){
+        action = e.target.parentElement.dataset.action
+        taskId = e.target.parentElement.dataset.itemId
+    }
+    else{
+        action = e.target.dataset.action
+        taskId = e.target.dataset.itemId
+    }
+    
+    switch(action){
+        case 'ready':
+            console.log('ready')
+            break
 
-   if(btn_name === 'btn_delete'){
+        case 'delete':
+            deleteTask(taskId)
+            break
 
-    lst.splice(index,1)
+        case 'detail':
+            console.log('detail')
+            break
 
-   }else if(btn_name === 'btn_ready'){
-
-   }
-  
-   if(btn_name === 'btn_delete' || btn_name === 'btn_ready'){
-
-    localStorage.setItem('taskList',  JSON.stringify(lst))
-
-    ListCards(lst)
-
-   }
-
+        case 'edit':
+            console.log('edit')
+            break
+    }
+    
+    e.stopPropagation();
 })
 
 function ListCards(lst){
 
-    let cards = ''
+    let container = document.getElementById('container_lst')
 
-    lst.forEach(element => {
+    if(!lst || lst.length <1){
 
-        cards += `<div class="p-2 row rounded justify-content-between bg-primary-subtle mt-3 ml-2 mr-2">
-                    <div class="col-3">
-                        <p class="fs-5">${element}</p>
-                    </div>
-                    <div class="row col-4 justify-content-between">
-                        <a href="#" data-item="${lst.indexOf(element)}" name=""btn_ready class="col-5 btn btn-success">
-                            Ready
-                        </a>
-                        <a href="#" data-item="${lst.indexOf(element)}" name="btn_delete" class="col-5 btn btn-danger">
-                            Delete
-                        </a>
-                    </div>
-                </div>`
+        container.innerHTML = `<div id="lst_row" class="row d-flex justify-content-around text-bg-light">
+                                <h2 class="text-center">No items</h2>
+                            </div>`
+    }else{
 
-        document.getElementById('lst_row').innerHTML = cards
+        container.innerHTML = ''
 
-    });
+        let table = document.createElement('table')
+        table.classList.add('table', 'table-striped', 'text-center', 'text-bordered')
+
+        let tbody = document.createElement('tbody')
+
+        let thead = document.createElement('thead')
+        let tr = document.createElement('tr')
+        let thTitle = document.createElement('th')
+        thTitle.innerText = 'Title'
+
+        let thDate = document.createElement('th')
+        thDate.innerText = 'Date'
+
+        let thActions = document.createElement('th')
+        thActions.innerText = 'Actions'
+
+        tr.append(thTitle, thDate, thActions)
+
+        thead.append(tr)
+
+        table.append(thead)
+
+        container.append(table)
+        
+        lst.forEach(element => {
+
+            let trBody = document.createElement('tr')
+
+            let tdBTitle = document.createElement('td')
+            tdBTitle.innerText = element.title
+
+            let tdBDate = document.createElement('td')
+            tdBDate.innerText = element.plannedDate
+
+            let tdBAction = document.createElement('td')
+            tdBAction.innerHTML = `<div class="d-flex justify-content-end">
+                                    <a href="#" data-item-id="${element.id}" data-action="ready" name="btn_ready" class="col-3 btn text-success">
+                                        <i class="bi bi-check2-circle"></i>
+                                    </a>
+                                    <a href="#" data-item-id="${element.id}" data-action="delete" name="btn_delete" class="col-3 btn text-danger">
+                                        <i class="bi bi-trash"></i>
+                                    </a>
+                                    <a href="#" data-item-id="${element.id}" data-action="detail" name="btn_detail" data-bs-toggle="modal" data-bs-target="#descModal" class="col-3 btn text-primary">
+                                        <i class="bi bi-card-text"></i>
+                                    </a>
+                                    <a href="#" data-item-id="${element.id}" data-action="edit" name="btn_edit" data-bs-toggle="modal" data-bs-target="#editModal" class="col-3 btn text-secondary">
+                                        <i class="bi bi-pencil-fill"></i>
+                                    </a>
+                                </div> `
+    
+            trBody.append(tdBTitle, tdBDate, tdBAction)
+
+            tbody.append(trBody)
+        });
+
+        table.append(tbody)
+    }
+}
+
+function generateId(){
+    let taskId = ''
+    
+    for(let i = 0; i < 10; i++){
+
+        taskId += char[Math.floor(Math.random() * char.length)]
+    }
+
+    return taskId
+}
+
+function deleteTask(id){
+
+   let data = localStorage.getItem('taskList')
+   
+   let taskList = JSON.parse(data)
+
+   let taskIndex = taskList.indexOf(taskList.find(t => t.id === id))
+
+   taskList.splice(taskIndex,1)
+
+   localStorage.setItem('taskList', JSON.stringify(taskList))
+
+   ListCards(taskList)
 }
